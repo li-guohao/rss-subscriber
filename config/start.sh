4f1f80d6dfa9df2799de968bbd2da290d566f424
li-guohao <liguohao_cn@qq.com>
2021-10-30 15:17:26 +0800

    添加参考配置

---
+++ config/start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+export RSS_SUBSCRIBER_SMTP_HOST=xxx
+export RSS_SUBSCRIBER_APP_TOKEN=xxx
+export RSS_SUBSCRIBER_SMTP_PASSWORD=xxx
+export RSS_SUBSCRIBER_SMTP_TARGET_EMAIL=xxx
+export RSS_SUBSCRIBER_SMTP_USERNAME=xxx
+
+/bin/java -jar /root/.rss-subscriber/rss-subscriber-1.0.jar
